d4d30d18733ab86cc68e3aff741fcc2538e205e6
sheikhhuzaif <sheikhhuzaif007@gmail.com>
2021-02-23 10:19:11 +0530

    password reset and password change

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
 
 from portfolio.forms import UserRegistrationForm
@@ -8,6 +9,16 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from portfolio.mail import send_joining_mail
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
 
 
 def register(request):
@@ -22,7 +33,7 @@ def register(request):
                 user = User(username=username, email=email)
                 user.set_password(raw_password=password)
                 user.save()
-                send_joining_mail(email,username)
+                send_joining_mail(email, username)
                 messages.success(request, "Account successfully created for " + username)
                 return redirect('login')
             else:
@@ -63,9 +74,70 @@ def home(request):
 
 @login_required(login_url='login')
 def edit(request):
-    print(request.POST)
-    return render(request, 'edit.html',{'range':range(1)})
+    return render(request, 'edit.html', {'range': range(1)})
+
+
+def display(request, username):
+    try:
+        user = User.objects.get(username=username)
+        context = {
+            'user': user
+        }
+        return render(request, 'view.html', context)
+    except:
+        return render(request, 'error.html')
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+@login_required(login_url='login')
+def settings(request):
+    if request.method=='POST':
+        if request.user.is_authenticated:
+            old=request.POST.get('old')
+            new=request.POST.get('new')
+            username=request.user
+            user=authenticate(request,username=username,password=old)
+            if user is not None:
+                user.set_password(new)
+                user.save()
+                user=authenticate(request,username=username,password=new)
+                login(request,user)
+                messages.info(request,'password change succesfully')
+                return redirect('home')
+            else:
+                messages.info(request,'old password is wrong')
+
+    return render(request,'settings.html')
+
+
+def password_reset_request(request):
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            emailid = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=emailid))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "password_reset_email.txt"
+                    context = {
+                        "email": user.email,
+                        'domain': '127.0.0.1:8000',
+                        'site_name': 'Website',
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "user": user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, context)
+                    try:
+                        send_mail(subject, email, 'admin@example.com', [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+                    return redirect("/password_reset/done/")
+    password_reset_form = PasswordResetForm()
+    return render(request=request, template_name="password_reset.html",
+                  context={"password_reset_form": password_reset_form})
--- portfolios/urls.py
+++ portfolios/urls.py
@@ -1,30 +1,29 @@
-"""portfolios URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 from portfolio import views
+from django.contrib.auth import views as auth_views
+app_name='portfolio'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.about, name='about'),
     path('home/', views.home, name='home'),
-    path('register/', views.register,  name='register'),
+    path('register/', views.register, name='register'),
     path('login/', views.loginPage, name='login'),
     path('accounts/', include('allauth.urls')),
     path('logout/', views.logoutUser, name='logout'),
-    path('edit/', views.edit, name='edit')
+    path('edit/', views.edit, name='edit'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
+    path('view/<str:username>/', views.display, name='view'),
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    path('settings/',views.settings,name='settings')
 ]
