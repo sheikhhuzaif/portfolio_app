f1fd0050cf98eacd9e4e1d112f0c02d90ebab240
Arafat Ashraf <64841392+arafatashraf@users.noreply.github.com>
2021-03-01 20:44:07 +0530

    edited form

--- portfolio/static/additionaljs/edit.js
+++ portfolio/static/additionaljs/edit.js
@@ -12,12 +12,25 @@ function addRowQual () {
   rem.parentNode.removeChild(rem);
   }
   var rowhtml = `<div class="row" id="rowqual`+addRowQual.count+`">
-                  <input type="text" name="degreename`+addRowQual.count+`" id="" class="" placeholder="Degree Name">
-                  <input type="text" name="degreedate`+addRowQual.count+`" id="" class="" placeholder="Year of Passing">
-                  <input type="text" name="degreeform`+addRowQual.count+`" id="" class="" placeholder="Board/University">
-                  <input type="text" name="degreegpa`+addRowQual.count+`" id="" class=""  placeholder="Percentage %">
-                  <input type="button" id="rmvbtnqual" value="Delete Row" onclick="removeRowQual(this)">
-                  </div>`;
+
+                <div class="form-floating mb-3">
+                <input type="text" name="degreename`+addRowQual.count+`" class="form-control" id="input1" placeholder="Degree Name">
+                <label style="color: #000;" for="input1">Degree Name</label>
+                </div>
+                <div class="form-floating mb-3">
+                <input type="text" name="degreedate`+addRowQual.count+`" class="form-control" id="input2" placeholder="Degree Date">
+                <label style="color: #000;" for="input2">Degree Date</label>
+                </div>
+                <div class="form-floating mb-3">
+                <input type="text" name="degreefrom`+addRowQual.count+`" class="form-control" id="input3" placeholder="Year of Passing">
+                <label style="color: #000;" for="input3">Year of Passing</label>
+                </div>
+                <div class="form-floating mb-3">
+                <input type="text" name="degreegpa`+addRowQual.count+`" class="form-control" id="input4" placeholder="GPA">
+                <label style="color: #000;" for="input4">GPA</label>
+                </div>
+                  <input type="button" class="btn btn-outline-danger btn-sm" id="rmvbtnqual" value="Delete Row" onclick="removeRowQual(this)">
+                 </div>`;
   document.querySelector('#qualcontent').insertAdjacentHTML(
     'beforeend',
     rowhtml
@@ -32,10 +45,10 @@ function removeRowQual (input) {
     var currentrow = '#rowqual'+addRowQual.count;
     document.querySelector(currentrow).insertAdjacentHTML(
       'beforeend',
-      '<input type="button" id="emvbtnqual" value="Delete Row" onclick="removeRowQual(this)">'
+      '<input type="button" class="btn btn-outline-danger btn-sm" id="emvbtnqual" value="Delete Row" onclick="removeRowQual(this)">'
     )
     }
-  }
+}
 
 //Skill Functions
 //Add row for Skills
@@ -47,8 +60,8 @@ function addRowSkill () {
     }
     var rowhtml = `<div class="row" id="rowskill`+addRowSkill.count+`">
                     <input type="text" name="skillname`+addRowSkill.count+`" placeholder="Skill name..." >
-                    <input type="button" id="rmvbtnskill" value="Delete Row" onclick="removeRowSkill(this)">
-                    </div>`;
+                    <input type="button" class="btn btn-outline-danger btn-sm" id="rmvbtnskill" value="Delete Row" onclick="removeRowSkill(this)">
+                   </div>`;
     document.querySelector('#skillcontent').insertAdjacentHTML(
       'beforeend',
       rowhtml
@@ -63,7 +76,7 @@ function removeRowSkill (input) {
     var currentrow = '#rowskill'+addRowSkill.count;
     document.querySelector(currentrow).insertAdjacentHTML(
       'beforeend',
-      '<input type="button" id="rmvbtnskill" value="Delete Row" onclick="removeRowSkill(this)">'
+      '<input type="button" class="btn btn-outline-danger btn-sm" id="rmvbtnskill" value="Delete Row" onclick="removeRowSkill(this)">'
     )
     }
 }
@@ -76,15 +89,27 @@ function addRowSocial () {
     var rem = document.getElementById('rmvbtnsocial');
     rem.parentNode.removeChild(rem);
     }
-    var rowhtml = `<div class="row" id="rowsocial`+addRowSocial.count+`">
-                    <input type="text" id="username`+addRowSocial.count+`" class="" placeholder="UserName">
-                    <input type="text" id="link`+addRowSocial.count+`" class="" placeholder="Link">  
-                    <input type="button" id="rmvbtnsocial" value="Delete Row" onclick="removeRowSocial(this)">
+    var rowhtml = `<div class="row g-2" id="rowsocial`+addRowSocial.count+`">
+                     <div class="col-md">
+                        <div class="">
+                          <input class="form-control" list="socialOptions" id="socialDataList" placeholder="Social Media">
+                          <datalist id="socialOptions">
+                          <option value="Facebook">
+                          <option value="Twitter">
+                          <option value="Instagram">
+                          <option value="GitHub">
+                          </datalist>
+                        </div>
+                      </div>
+                     <div class="col-md">
+                     <div class="input-group mb-3">
+                     <span class="input-group-text" id="basic-addon1">@</span>
+                     <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                     </div>
+                     </div>
+                     <input type="button" class="btn btn-outline-danger btn-sm" id="rmvbtnsocial" value="Delete Row" onclick="removeRowSocial(this)">
                    </div>`;
-    document.querySelector('#socialcontent').insertAdjacentHTML(
-      'beforeend',
-      rowhtml
-    )
+    document.querySelector('#socialcontent').insertAdjacentHTML('beforeend',rowhtml);
 }
 
 //Remove row for Skills
@@ -95,7 +120,7 @@ function removeRowSocial (input) {
     var currentrow = '#rowsocial'+addRowSocial.count;
     document.querySelector(currentrow).insertAdjacentHTML(
       'beforeend',
-      '<input type="button" id="rmvbtnsocial" value="Delete Row" onclick="removeRowSocial(this)">'
+      '<input type="button" class="btn btn-outline-danger btn-sm" id="rmvbtnsocial" value="Delete Row" onclick="removeRowSocial(this)">'
     )
     }
 }
