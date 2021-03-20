6e40f89630557a3c6b4620bdbddc29e3a4d912ba
sheikhhuzaif <sheikhhuzaif007@gmail.com>
2021-03-20 12:59:46 +0530

    o

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -31,5 +31,3 @@ urlpatterns = [
     path('contact/',views.contact,name='contact')
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
